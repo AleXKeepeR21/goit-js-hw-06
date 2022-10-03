1a13350e67c29e112a284854c5c83c90c567426a
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-10-03 15:39:27 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,29 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const makeItems = ingredients => {
+  return ingredients.map(ingredient => {
+    const navItemRef = document.createElement('li');
+    navItemRef.textContent = ingredient;
+    navItemRef.classList.add('item');
+    return navItemRef;
+  });
+}
+
+const itemsEl = makeItems(ingredients);
+
+ingredientsRef.append(...itemsEl);
+
+console.log(...itemsEl)
+
+// const navItemRef = document.createElement('li');
+// navItemRef.classList.add('item');
+// navItemRef.textContent = 'Potatoes';
+// console.log(navItemRef);
+
+
+
+
