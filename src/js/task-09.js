05a5e1acdab1d48059f7a65d3f3bfa35dda6ee05
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-10-06 18:22:49 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
+const buttonRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+buttonRef.addEventListener('click', (event) => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanRef.textContent = randomColor;
+
+  })
+
+
+
