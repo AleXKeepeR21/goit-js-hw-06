e32d357b42d5da8eb0aa555364ff25eeb5c90900
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-10-06 16:30:26 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
+const loginFormRef = document.querySelector('.login-form');
+const inputs = document.querySelectorAll('input');
+
+loginFormRef.addEventListener("submit", (event) => {
+    event.preventDefault();
+        
+    const { email } = event.target.elements;
+    const { password } = event.target.elements;
+
+    const data = {
+        email: email.value,
+        password: password.value
+    };
+
+    inputs.forEach(input => {
+    if (input.value.trim() === '')
+    {        alert("Увага! Заповніть усі поля.");
+    }
+    });
+
+    event.target.reset();
+    console.log(data)
+})
+
+
 
